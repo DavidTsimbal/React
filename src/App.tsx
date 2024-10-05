c0d5cf0f302e9a190b36d62cb49de6210700a665
David Tsimbal <villianer24@gmail.com>
2024-10-05 03:32:59 +0300

    Pokemon Info

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import './App.css';
 import pokemon from './pockemon.json';
 import PropTypes from 'prop-types';
 
-const PokemonRow = ( {pokemon} ) => (
+const PokemonRow = ( {pokemon, onSelect} ) => (
   <tr>
     <td>{pokemon.name.english}</td>
     <td>{pokemon.type.join(', ')}</td>
+    <td><button onClick={() => onSelect(pokemon)}>Select</button></td>
   </tr>
 );
 
@@ -17,13 +18,41 @@ PokemonRow.propTypes = {
     }),
     type: PropTypes.arrayOf(PropTypes.string),
   }),
+  onSelect: PropTypes.func,
+};
+
+const PokemonInfo = ({name, base}) => (
+  <div className='selection'>
+    <h1>{name.english}</h1>
+    <table>
+      {Object.keys(base).map((key) => (
+        <tr key={key}>
+          <td>{key}</td>
+          <td>{base[key]}</td>
+        </tr>
+      ))}
+    </table>
+  </div>
+);
+
+PokemonInfo.propTypes = {
+  name: PropTypes.shape({
+    english: PropTypes.string,
+  }),
+  base: PropTypes.shape({
+    HP: PropTypes.number.isRequired,
+    Attack: PropTypes.number.isRequired,
+    Defence: PropTypes.number.isRequired,
+    "Sp. Attack": PropTypes.number.isRequired,
+    "Sp. Defence": PropTypes.number.isRequired,
+    Speed: PropTypes.number.isRequired,
+  }),
 }
 
 function App() {
 
   const [filter, setFilter] = useState("");
-
- 
+  const [selectedItem, setSelectedItem] = useState();
 
   return (
   
@@ -35,6 +64,7 @@ function App() {
       }}
       >
         <h1 className="title">Pokemon Search</h1>
+        <div>
         <input 
           type="text" 
           placeholder='search' 
@@ -50,10 +80,12 @@ function App() {
           </thead>
           <tbody>
             {  pokemon.filter((pokemon) => pokemon.name.english.toLowerCase().includes(filter.toLowerCase())).map((pokemon) => (
-              <PokemonRow pokemon={pokemon} key={pokemon.id}></PokemonRow>
+              <PokemonRow pokemon={pokemon} key={pokemon.id} onSelect={(pokemon) => setSelectedItem(pokemon)}></PokemonRow>
             ))}
           </tbody>
         </table>
+        </div>
+        {selectedItem && <PokemonInfo {...selectedItem} />}
       </div>
     
   );
