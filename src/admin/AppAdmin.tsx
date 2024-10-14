3dc0c7873222dbe477628e72c04b4f68011f501f
David Tsimbal <villianer24@gmail.com>
2024-10-14 09:19:04 +0300

    Admin page. Switching tables

--- src/admin/AppAdmin.tsx
+++ src/admin/AppAdmin.tsx
@@ -3,85 +3,79 @@ import { useEffect } from 'react';
 import React from 'react';
 import Header from "../components/Header.tsx";
 import Footer from "../components/Footer.tsx";
-import Ticket from "./components/Ticket.tsx";
-import Total from "./components/Total.tsx";
+import { string } from 'prop-types';
+import { table } from 'console';
 
 
 function App() {
   
-  const [ticketPrice, setTicketPrice] = useState(0);
-  const [totalPrice, setTotalPrice] = useState(0);
-  const [selectedQuantity, setSelectedQuantity] = useState(1);
-  const [totalQuantity, setTotalQuantity] = useState(52);
-  const [ticketList, setTicketList] = useState([1]);
-
-  useEffect(() => {
-
-    console.log('ticketList:', ticketList);
-    console.log("selectedQuantity:", selectedQuantity);
-    console.log("totalPrice", totalPrice);
-
-  },[ticketList, selectedQuantity, totalPrice]);
+  const [data, setData] = useState();
+  const [tables, setTables] = useState(['']);
+  const [selectedTable, setSelectedTable] = useState('');
+  const [filter, setFilter] = useState();
   
-
   const [isLoaded, setIsLoaded] = useState(false);
 
   async function LoadHandler(){
     if(!isLoaded){
-      const LoadRequest = new Request("https://veyer-conf.ru/shop-info.php", { method: 'GET', });
+      const LoadRequest = new Request("https://veyer-conf.ru/admin-info.php", { method: 'GET', });
       
       const response = await fetch(LoadRequest);
       const result = await response.json();
       console.log("Success:", result);
     
-      setTicketPrice(result["price"]);
-      setTotalPrice(result["price"]);
-      setTotalQuantity(result["quantity"]);
+      setData(result);
+      setTables(Object.keys(result));
+      setSelectedTable(Object.keys(result)[0]);
+
+      console.log("Tables:", tables);
+      console.log("Selected table:", selectedTable);
+      console.log(data);
       
       setIsLoaded(true);
     }
   }
 
-
-
-  function delTicket(id: number){
-
-    if(selectedQuantity > 0){
-      
-      setTicketList(ticketList.filter((item) => item !== id));
-  
-      console.log("deleted ticket id:", id);
-  
-      const newQuantity = selectedQuantity - 1;
-      setSelectedQuantity(newQuantity);
-  
-      const newTotalPrice = selectedQuantity * ticketPrice;
-      setTotalPrice(newTotalPrice); 
-    
-    }
-
+  function searchHandle(event){
+    setFilter(event.target.value);
   }
 
-
-  const [idcounter, setIdcounter] = useState(1);
-
-  function addTicket(){
-
-    if(ticketList.length < totalQuantity){
-
-      const newTicketId = idcounter + 1;
-      setIdcounter(idcounter + 1);
-      
-      setTicketList(ticketList.concat([newTicketId]));
-  
-      const newQuantity = selectedQuantity + 1;
-      setSelectedQuantity(newQuantity);
-      
-      const newTotalPrice = selectedQuantity * ticketPrice;
-      setTotalPrice(newTotalPrice);
-    }
-
+  function selectTable(event){
+    setSelectedTable(event.target.value);
   }
+
+  function nullCheck(variable){
+    if(variable != (null || undefined)){
+      return variable;
+    }
+    else{
+      return '';
+    }
+  }
+
+ function mapTable(data){
+    if(data){
+      return (
+        <table>
+        <thead>
+                {Object.keys(data[selectedTable][0]).map(key => (
+                  <th>{key}</th>
+                ))}
+        </thead>
+        <tbody>
+                {data[selectedTable].map(row => (
+                  <tr>
+                    {Object.keys(row).map(key => (
+                      <td><input type="text" name={row[key]} id={row[key]} value={row[key]} /></td>
+                    ))}
+                  </tr>
+                ))}
+        </tbody>
+        </table>
+      );
+    }
+  }
+  
 
 
   return (
@@ -90,35 +84,39 @@ function App() {
       <Header></Header>
 
       <main className="tickets">
-      <div className="paddings">
+      <div className="paddings admin-profile-grid">
 
-        <div>
-          <h1>Здравствуйте!</h1>
-          <p>Здесь вы можете приобрести билеты на Веер. Каждый билет - случайная игральная карта. Назовите её на входе и погружайтесь в мир кардистри! </p>
-        </div>
+          <div></div>
 
-        <hr id="breakline"></hr>
-
-        <p id="tickets-counter">Билетов осталось: {totalQuantity}/52</p>
-
-        <form className="tickets-template" id="tickets-form" action="https://veyer-conf.ru/payment.php" method="post">
-        
           <div>
-              
-              <div id="tickets-list">
-                  {ticketList.map((id) => (
-                    <Ticket id={id} ticketPrice={ticketPrice} idcounter={id} key={id} delTicket={delTicket}></Ticket>
-                  ))}
-              </div>
-  
-            <button id="plus-button" form="" onClick={() => addTicket()}>+</button>
-  
-          </div>
-  
-          <Total totalPrice={selectedQuantity * ticketPrice}></Total>
-          
-        </form>
+            <div className='header-grid'>
+              <h1>Панель администратора</h1>
+              <button id='save-button'>СОХРАНИТЬ</button>
+            </div>
 
+            <hr id="breakline2"></hr>
+
+            <p>{selectedTable}</p>
+          </div>
+
+          <div></div>
+
+          <div>
+            <label htmlFor="search">Поиск:</label>
+            <br />
+            <input type="text" id='search' onChange={(e) => searchHandle(e)} placeholder='ПОИСК'/>
+          </div>
+
+          <div>
+            {tables.map(tableName => (
+              <div>
+                <input type="radio" id={tableName} onChange={(e) => selectTable(e)} name="table" value={tableName}/>
+                <label htmlFor={tableName}>{tableName}</label>
+              </div>
+            ))}
+          </div>
+          
+          {mapTable(data)}
 
       </div>
     </main>
@@ -129,48 +127,5 @@ function App() {
   );
 }
 
-// function App() {
-
-
-//   return (
-  
-//     <div onLoad={LoadHandler}>
-//       <Header></Header>
-
-//       <main className="tickets">
-//       <div className="paddings">
-
-//         <div>
-//           <h1>Здравствуйте!</h1>
-//           <p>Здесь вы можете приобрести билеты на Веер. Каждый билет - случайная игральная карта. Назовите её на входе и погружайтесь в мир кардистри! </p>
-//         </div>
-
-//         <hr id="breakline"></hr>
-
-//         <p id="tickets-idcounter">Билетов осталось: ?/52</p>
-
-//         <form className="tickets-template" id="tickets-form" action="https://veyer-conf.ru/payment.php" method="post">
-        
-//           <div>
-
-//             <Ticket ticketPrice={ticketPrice}></Ticket>
-  
-//             <button id="plus-button" form="">+</button>
-  
-//           </div>
-  
-//           <Total totalPrice={totalPrice}></Total>
-          
-//         </form>
-
-
-//       </div>
-//     </main>
-
-//       <Footer></Footer>
-//     </div>
-    
-//   );
-// }
 
 export default App;
